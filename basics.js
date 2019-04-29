ff98bf99574080abd255c518deef473199f37b96
Maarten <maartenvdh@live.be>
2019-04-29 17:06:05 +0400

    new update

---
+++ basics.js
@@ -0,0 +1,7 @@
+var container1 = document.getElementById("container1");
+var container2 = document.getElementById("container2");
+var container3 = document.getElementById("container3");
+var container4 = document.getElementById("container4");
+
+var dpButton = document.getElementById("dpButton")
+var dvButton = document.getElementById("dvButton")
